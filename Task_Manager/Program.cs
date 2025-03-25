80957611b5a0e831785af073aec082d413702131
d3s <rn.oscar11@gmail.com>
2025-03-24 21:56:43 -0600

    Codigo inicial

---
+++ Task_Manager/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Task_Manager;
+
+class Program
+{
+    static void Main()
+    {
+        int opcion;
+        var task = new Task_User();
+        do
+        {
+            Console.WriteLine("Bienvenido al gestor de tareas. Indique la opcion que desee: ");
+            Console.Write("1. Anadir tarea.\n" +
+                          "2. Listar tareas.\n" +
+                          "3. Actualizar tarea.\n" +
+                          "4. Eliminar tarea.\n" +
+                          "5. Salir.\n");
+
+            try
+            {
+                opcion = int.Parse(Console.ReadLine());
+                Console.WriteLine(opcion);
+                switch (opcion)
+                {
+                    case 1:
+                        task.CreateTask();
+                        break;
+                    case 2:
+                        Console.WriteLine("opcion 2.");
+                        break;
+                    case 3:
+                        Console.WriteLine("opcion 3.");
+                        break;
+                    case 4:
+                        Console.WriteLine("opcion 4.");
+                        break;
+                    case 5:
+                        Console.WriteLine("opcion 5.");
+                        break;
+                    default:
+                        Console.WriteLine("Seleccione una opcion valida.");
+                        break;
+                }
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Ingrese una opcion valida (Numeros del 1 al 5)");
+                opcion = 0;
+            }
+            
+        }while (opcion != 5);
+    }
+}
---
+++ Task_Manager/Task.cs
@@ -0,0 +1,163 @@
+using Newtonsoft.Json;
+using System.Globalization;
+using System.IO;
+using System;
+namespace Task_Manager;
+
+public class Task_User
+{
+    public Task_User(){}
+    public string Tittle; 
+    public string Description;
+    public enum Priority
+    {
+        LOW = 3,
+        MEDIUM = 2,
+        HIGH = 1,
+    }
+    public DateTime StartTime;
+    public DateTime EndTime;
+
+    public void CreateTask()
+    {
+        Console.Write("Ingrese el titulo: ");
+        Tittle = ValidateString(Console.ReadLine(), "Titulo");
+        Console.Write("Ingrese la descripcion: ");
+        Description = ValidateString(Console.ReadLine(), "Descripcion");
+        bool check = false;
+        do
+        {
+            Console.Write("Seleccione la prioridad (Indique el numero o el nombre): \n");
+            foreach (var value in Enum.GetValues(typeof(Priority)))
+            {
+                Console.WriteLine($"{(int)value} - {value}");
+            }
+            Console.Write("Prioridad: ");
+            string input = Console.ReadLine().ToUpper();
+            int x;
+            Priority priority;
+            
+            if (Int32.TryParse(input, out x))
+            {
+                check = Enum.IsDefined(typeof(Priority), x);
+            }
+            else
+            {
+                check = Enum.TryParse<Priority>(input, out priority);
+            }
+            
+            if (!check)
+            {
+                Console.WriteLine("Ingrese una prioridad valida.");
+            }
+        }while(!check);
+        
+        Console.Write("Ingrese la fecha de inicio (format DD/MM/YYYY): ");
+        StartTime = ValidateDate(Console.ReadLine());
+        
+        Console.Write("Ingrese la fecha de fin (format DD/MM/YYYY): ");
+        EndTime = ValidateDate(Console.ReadLine());
+        
+        writeTask(Tittle,Description,StartTime,EndTime);
+                
+    }
+
+    private DateTime ValidateDate(string date)
+    {
+        DateTime dateTime;
+        string datePattern = "dd/MM/yyyy";
+        while (true)
+        {
+            if (string.IsNullOrEmpty(date))
+            {
+                Console.WriteLine("La fecha no puede estar vacio.");
+            }else if (!DateTime.TryParseExact(date, datePattern, null, DateTimeStyles.None, out dateTime))
+            {
+                Console.WriteLine($"Formato de fecha invalido, el formato debe de ser: {datePattern} ");
+            }
+            else
+            {
+                break;
+            }
+            Console.Write($"Ingrese la fecha nuevamente (formato {datePattern}): ");
+            date = Console.ReadLine();
+        }
+        return dateTime;
+    }
+
+    private string ValidateString(string input, string fieldName)
+    {
+        while (true)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine($"{fieldName} no puede estar vacio.");
+                Console.Write($"Ingrese el {fieldName} nuevamente: ");
+                input = Console.ReadLine();
+            }
+            else
+            {
+                break;
+            }
+            
+        }
+        return input;
+    }
+
+    private void writeTask(string title, string description, DateTime startTime, DateTime endTime)
+    {
+        List<Task_User> tasks = new List<Task_User>();
+        
+        var pathFile = @"/home/d3s/RiderProjects/Task_Manager/Task_Manager/tasks.json";
+
+        try
+        {
+            if (File.Exists(pathFile))
+            {
+                tasks = JsonConvert.DeserializeObject<List<Task_User>>(pathFile) ?? new List<Task_User>();
+            }
+            else
+            {
+                File.WriteAllText(pathFile, "");
+            }
+
+            tasks.Add(new Task_User()
+            {
+                Tittle = title,
+                Description = description,
+                StartTime = startTime,
+                EndTime = endTime
+            });
+        
+            string json = JsonConvert.SerializeObject(tasks);
+            File.WriteAllText(pathFile, json);
+        }
+        catch (Exception ex)
+        {
+            var pathLogs = @"/home/d3s/RiderProjects/Task_Manager/Task_Manager/tasks.json";
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    private void readTask()
+    {
+        List<Task_User> tasks = new List<Task_User>();
+        var pathFile = @"/home/d3s/RiderProjects/Task_Manager/Task_Manager/tasks.json";   
+        
+        if (File.Exists(pathFile))
+        {
+            FileInfo fileInfo = new FileInfo(pathFile);
+            
+            if (fileInfo.Length > 0)
+            {
+                
+            }
+            tasks = JsonConvert.DeserializeObject<List<Task_User>>(pathFile) ?? new List<Task_User>();
+            
+        }
+        else
+        {
+            File.WriteAllText(pathFile, "");
+        }
+    }
+}
